ffcfccbdb0caf875da0fc929c0363e92cd2ab735
Nuru <access2abey@gmail.com>
2021-05-19 22:31:50 +0100

    load dataset as an image datastore

---
+++ xray.m
@@ -0,0 +1,9 @@
+%Close all open figures
+close all
+%Clear the workspace
+clear
+%Clear the command window
+clc
+
+% load the X-Ray dataset as an image datastore
+imds = imageDatastore('medicalmnist', 'IncludeSubfolders', true, 'LabelSource','foldernames')
